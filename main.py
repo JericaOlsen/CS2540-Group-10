c91fe5d51aaf8d2e923f67127940585eaeccf451
Jerica Olsen <114546897+JericaOlsen@users.noreply.github.com>
2025-01-28 18:58:45 -0700

    Update main.py
    Adding in branch functions

--- main.py
+++ main.py
@@ -10,7 +10,35 @@ class CPU:
 
   def write():
     pass
+  def branch(self,index):
+    """_summary_
 
+    Args:
+        index (int): integer specifying where memory should branch
+    """
+    self.cr = index
+    print("You have branched to location" + str(index))
+
+  def branchneg(self,index):
+    """_summary_
+      If accumulator is negative branch to a specific location
+    """
+    if self.accumulator < 0:
+      self.branch(index)
+    else:
+      print("Accumulator isn't negative, there was no branching ")
+
+  def branchzero(self,index):
+    """_summary_
+
+      If accumulator is zero branch to a specific location
+    """
+    if self.accumulator == 0:
+      self.branch(index)
+    else:
+        print("Accumulator isn't zero, there was no branching ")
+
+  
 class Memory:
   def __init__(self, legnth = 100):
     self.length = length
