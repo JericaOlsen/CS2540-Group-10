9fd9a32fd55fca6f94dfcbd1b5558d90b9d5b035
Jerica Olsen <114546897+JericaOlsen@users.noreply.github.com>
2025-02-06 17:05:20 -0700

    Update and rename branch_functions.py to main.py
    Updating name for commit

--- main.py
+++ main.py
@@ -1,15 +1,4 @@
-class CPU:
-  def __init__(self, memory):
-    self.accumulator: int = 0
 
-  def execute():
-    pass
-
-  def read():
-    pass
-
-  def write():
-    pass
   def branch(self,index):
     """_summary_
 
@@ -37,29 +26,3 @@ class CPU:
       self.branch(index)
     else:
         print("Accumulator isn't zero, there was no branching ")
-
-  
-class Memory:
-  def __init__(self, legnth = 100):
-    self.length = length
-    self.words = []
-
-def get(index) -> int:
-  pass
-
-def set(index: int, value: int)
-
-def main(args):
-  memory = Memory()
-  cpu = CPU(memory)
-
-  with open(args[1], 'w') as program_file:
-    instructions = []
-    for line in program_file.readlines():
-      instructions.append(int(line))
-
-    cpu.execute(instructions)
-
-if __name__ == '__main__':
-  import sys
-  sys.exit(main(sys.argv))
