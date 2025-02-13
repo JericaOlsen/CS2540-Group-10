ebf425460283cc81d83038ecfb95217d928c3fab
Jerica Olsen <114546897+JericaOlsen@users.noreply.github.com>
2025-02-12 20:41:59 -0700

    Update and rename id7_test2.py to test_id7_test2.py

--- test_id7_test2.py
+++ test_id7_test2.py
@@ -1,14 +1,16 @@
-import main
+import computer
+from io import StringIO
 
-def test_teacher72():
+def test_teacher72(monkeypatch):
     inst = [+1060, +1061, +1062, +1063, +2060, +3361, +3362, +3363, +2164, +1164, +4300]
 
-    memory = main.Memory()
-    cpu = main.CPU(memory)
+    memory = computer.Memory()
+    cpu = computer.CPU(memory)
 
     for i, inst in enumerate(inst):
         memory.set(i, inst)
 
+    monkeypatch.setattr('sys.stdin', StringIO("24\n24\n24\n24\n"))
     #User inputs 24 four times
     cpu.execute()
     assert cpu.accumulator == 331776 
