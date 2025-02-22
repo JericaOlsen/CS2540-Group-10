f7300b11b420de7f25df9592fab7b6b96abd3c6e
jsbrewer <195831175+jsbrewer@users.noreply.github.com>
2025-02-22 00:02:44 -0700

    Update id9_test2.py

--- id9_test2.py
+++ id9_test2.py
@@ -1,8 +1,8 @@
-from main import Memory, CPU
+from computer import Memory, CPU
 
 def test_id9_2():
     memory = Memory()
-    cpu = CPU(memory)
+    cpu = CPU(memory, print, input)
 
     for i, instruction in enumerate([1100, 1101, 1102, 4300]):
         memory.set(i, instruction)
